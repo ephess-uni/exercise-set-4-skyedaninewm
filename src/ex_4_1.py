a2b91e78f30261f16bd83705506a1c9d90dee2c5
skyedaninewm <143444435+skyedaninewm@users.noreply.github.com>
2023-10-10 10:08:21 -0500

    4_1 and 4_2, etc

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -17,7 +17,8 @@ def num_shutdowns(logfile):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
+    numbers = len(get_shutdown_events(logfile))
+    return numbers
 
 
 # >>>> The code below will call your function and print the results
